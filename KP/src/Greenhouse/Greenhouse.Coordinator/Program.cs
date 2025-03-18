4397b9a36c5e47c485d5440ce13d42448f6e9d18
libsyfer <libsyfer@gmail.com>
2025-03-18 23:26:09 +0300

    Созданы необходимые ендпоинты. Добавлен в DI сервис координирования выращивания

--- KP/src/Greenhouse/Greenhouse.Coordinator/Program.cs
+++ KP/src/Greenhouse/Greenhouse.Coordinator/Program.cs
@@ -1,4 +1,8 @@
+using Greenhouse.Coordinator;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<CoordinatorService>();
 
 builder.Services.AddSwaggerGen();
 
@@ -9,4 +13,25 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/status", (CoordinatorService coordinatorService) =>
+{
+    return Results.Ok(new
+    {
+        Status = coordinatorService.IsBuzy ? "buzy" : "free"
+    });
+})
+.WithDisplayName("status")
+.WithOpenApi();
+
+app.MapPost("/growing", (Guid paramsId) =>
+{
+    return Results.Ok(new
+    {
+        GrowindId = Guid.NewGuid()
+    });
+})
+.WithDisplayName("growing")
+.WithOpenApi();
+
 app.Run();
