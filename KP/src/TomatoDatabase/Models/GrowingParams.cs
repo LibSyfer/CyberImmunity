a10486d6fedcaa8f28bcba858844cb6b7b4dd7b7
libsyfer <libsyfer@gmail.com>
2025-03-19 01:23:34 +0300

    Исправлена кодировка файла

--- KP/src/TomatoDatabase/Models/GrowingParams.cs
+++ KP/src/TomatoDatabase/Models/GrowingParams.cs
@@ -1,4 +1,4 @@
-﻿namespace TomatoDatabase.Web.Models
+namespace TomatoDatabase.Web.Models
 {
     public class GrowingParams
     {
@@ -6,15 +6,15 @@
 
         public Guid TomatoId { get; set; }
 
-        public double LightIntensity { get; set; }                 //Интенсивность света в люксах
+        public double LightIntensity { get; set; }                  // Интенсивность света в люксах
 
         public int LightDuration { get; set; }                      //Продолжительность светового дня в часах
         
-        public double TemperatureDay { get; set; }                  //Температура днем в °C
+        public double TemperatureDay { get; set; }                  //Температура днем в цельсиях
 
-        public double TemperatureNight { get; set; }                //Температура ночью в °C
+        public double TemperatureNight { get; set; }                //Температура ночью в цельсиях
         
-        public double HumidityLevel { get; set; }                   //Уровень влажности в %
+        public double HumidityLevel { get; set; }                   //Уровень влажности в процентах
         
         public double WateringFrequency { get; set; }               //Частота полива в днях
 
