853f2c3de51f507bfb2cb34278a0481373226ba7
libsyfer <libsyfer@gmail.com>
2025-03-20 06:56:05 +0300

    Добавлены поля базового события

--- KP/src/Greenhouse/Greenhouse.EventBus/Events/IntegrationEvent.cs
+++ KP/src/Greenhouse/Greenhouse.EventBus/Events/IntegrationEvent.cs
@@ -1,8 +1,18 @@
-using System;
+using System.Text.Json.Serialization;
 
 namespace Greenhouse.EventBus.Events;
 
 public class IntegrationEvent
 {
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreationDate = DateTime.UtcNow;
+    }
 
+    [JsonInclude]
+    public Guid Id { get; set; }
+
+    [JsonInclude]
+    public DateTime CreationDate { get; set; }
 }
