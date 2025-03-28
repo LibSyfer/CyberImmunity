433f50e734b95b87ac5cd790465fa9caa1d808af
ArtemKD <Artyomkozlov2001@gmail.com>
2025-03-28 15:30:54 +0300

    Добавлен интерфейс базового хендлера сообщений. Добавлено наследование типизированного обработчика от базового

--- KP/src/Greenhouse/Greenhouse.MessageBus/Abstractions/IIntegrationMessageHandler.cs
+++ KP/src/Greenhouse/Greenhouse.MessageBus/Abstractions/IIntegrationMessageHandler.cs
@@ -2,9 +2,16 @@
 
 namespace Greenhouse.MessageBus.Abstractions
 {
-    public interface IIntegrationMessageHandler<in TIntegrationMessage>
+    public interface IIntegrationMessageHandler<in TIntegrationMessage> : IIntegrationMessageHandler
         where TIntegrationMessage : IntegrationMessage
     {
+        Task IIntegrationMessageHandler.Handle(IntegrationMessage message, CancellationToken cancellationToken) => Handle((TIntegrationMessage)message, cancellationToken);
+
         Task Handle(TIntegrationMessage message, CancellationToken cancellationToken = default);
+    }
+
+    public interface IIntegrationMessageHandler
+    {
+        Task Handle(IntegrationMessage message, CancellationToken cancellationToken = default);
     }
 }
