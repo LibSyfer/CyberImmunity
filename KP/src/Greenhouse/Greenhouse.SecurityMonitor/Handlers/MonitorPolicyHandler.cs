0e3856b05e75103fade112db8b598b5165eaa766
libsyfer <libsyfer@gmail.com>
2025-03-31 01:48:38 +0300

    Составлены необходимые политики бкзопасности

--- KP/src/Greenhouse/Greenhouse.SecurityMonitor/Handlers/MonitorPolicyHandler.cs
+++ KP/src/Greenhouse/Greenhouse.SecurityMonitor/Handlers/MonitorPolicyHandler.cs
@@ -1,40 +1,59 @@
 ﻿using Greenhouse.MessageBus.Abstractions;
+using Greenhouse.MessageBus.Messages.SeedPlantingSystem;
+using Greenhouse.MessageBus.Messages.TomatoDbConnectionModule;
 using Greenhouse.MessageBus.RabbitMQ.Extensions;
+using Greenhouse.Share;
 
 namespace Greenhouse.SecurityMonitor.Handlers
 {
     public class MonitorPolicyHandler : IMonitorMessageHandler
     {
         private readonly ILogger<MonitorPolicyHandler> _logger;
+        private readonly IMonitorMessageBus _monitorMessageBus;
 
-        public MonitorPolicyHandler(ILogger<MonitorPolicyHandler> logger)
+        public MonitorPolicyHandler(ILogger<MonitorPolicyHandler> logger, IMonitorMessageBus monitorMessageBus)
         {
             _logger = logger;
+            _monitorMessageBus = monitorMessageBus;
         }
 
-        public Task Handle(IDictionary<string, object?> metadata, ReadOnlyMemory<byte> payload, CancellationToken cancellationToken = default)
+        public async Task Handle(IDictionary<string, object?> metadata, ReadOnlyMemory<byte> payload, CancellationToken cancellationToken = default)
         {
-            _logger.LogInformation($"Handling message metadata: {metadata}");
+            _logger.LogInformation($"Получено сообщение, проверка политик...");
 
             var monitorHeaders = metadata.ReadMonitorHeaders();
-
             var authorizeAction = false;
 
-            if (monitorHeaders.ActionName.Equals("Test", StringComparison.OrdinalIgnoreCase) 
-                && monitorHeaders.Source.Equals("Coordinator", StringComparison.OrdinalIgnoreCase)
-                && monitorHeaders.Destination.Equals("Coordinator", StringComparison.OrdinalIgnoreCase))
+            if (monitorHeaders.ActionName.Equals(nameof(GetGrowingParamsCommand), StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Source.Equals(GreenhouseServicesNames.CoordinatorModule, StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Destination.Equals(GreenhouseServicesNames.TomatoDbConnectionModule, StringComparison.OrdinalIgnoreCase))
+                authorizeAction = true;
+
+            if (monitorHeaders.ActionName.Equals(nameof(GrowingParamsResultCommand), StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Source.Equals(GreenhouseServicesNames.TomatoDbConnectionModule, StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Destination.Equals(GreenhouseServicesNames.CoordinatorModule, StringComparison.OrdinalIgnoreCase))
+                authorizeAction = true;
+
+            if (monitorHeaders.ActionName.Equals(nameof(StartSeedingCommand), StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Source.Equals(GreenhouseServicesNames.CoordinatorModule, StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Destination.Equals(GreenhouseServicesNames.SeedPlantingModule, StringComparison.OrdinalIgnoreCase))
+                authorizeAction = true;
+
+            if (monitorHeaders.ActionName.Equals(nameof(SeedingFinishCommand), StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Source.Equals(GreenhouseServicesNames.SeedPlantingModule, StringComparison.OrdinalIgnoreCase)
+                && monitorHeaders.Destination.Equals(GreenhouseServicesNames.CoordinatorModule, StringComparison.OrdinalIgnoreCase))
                 authorizeAction = true;
 
             if (authorizeAction)
             {
-                _logger.LogInformation($"[Action: {monitorHeaders.ActionName}] [Source: {monitorHeaders.Source}] [Destination: {monitorHeaders.Destination}] allowed");
+                _logger.LogInformation($"Действие [Action: {monitorHeaders.ActionName}] [Source: {monitorHeaders.Source}] [Destination: {monitorHeaders.Destination}] разрешено политиками безопасности");
+
+                await _monitorMessageBus.ResendAsync(monitorHeaders.Destination, monitorHeaders.ActionName, payload, cancellationToken);
             }
             else
             {
-                _logger.LogWarning($"[Action: {monitorHeaders.ActionName}] [Source: {monitorHeaders.Source}] [Destination: {monitorHeaders.Destination}] rejected");
+                _logger.LogWarning($"Действие [Action: {monitorHeaders.ActionName}] [Source: {monitorHeaders.Source}] [Destination: {monitorHeaders.Destination}] запрещено политиками безопасности");
             }
-
-            return Task.CompletedTask;
         }
     }
 }
